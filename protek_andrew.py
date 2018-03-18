7c43a0f0a07b81258f8c53ee4e1e580204f3f05e
kolya95 <kolya95@users.noreply.github.com>
2018-03-18 18:51:51 +0300

    added telegram bots code

---
+++ protek_andrew.py
@@ -0,0 +1,343 @@
+import pandas as pd
+import numpy as np
+from emoji import emojize
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, \
+    ReplyKeyboardMarkup, KeyboardButton, ChatAction, InputMediaPhoto, LabeledPrice
+from telegram.ext import Updater, CommandHandler, ConversationHandler, \
+    MessageHandler, Filters, RegexHandler, CallbackQueryHandler, PreCheckoutQueryHandler
+from pydub import AudioSegment
+from google.cloud import speech
+import os
+import io
+import os
+from matplotlib import pyplot as plt
+import pymystem3
+import pymorphy2
+import nltk
+from nltk.stem.snowball import RussianStemmer
+import re
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.corpus import brown
+morfer = pymorphy2.MorphAnalyzer()
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './voicos-ef9646a397b1.json'
+
+TOKEN = '528649220:AAELfBOWBgv88CvGlIHy_1n8OpuTZh8U9_s'
+
+MAIN, CHOICE = range(2)
+
+###
+link_table = pd.read_csv('./final_out.csv', index_col=0)
+link_table['clear_symptoms'] = link_table['clear_symptoms'].apply(lambda yy: [x[1:-1] for x in yy[1:-1].split(', ')])
+link_table['symptoms'] = link_table['symptoms'].apply(lambda yy: [x[1:-1] for x in yy[1:-1].split(', ')])
+
+
+def add_ngrams(list_of_ends):
+    list_of_ngramms = []
+    for i in range(len(list_of_ends[-1])):
+        list_of_ngramms.append(tuple(list_of_ends[j][i] for j in range(len(list_of_ends))))
+    return Counter(list_of_ngramms)
+
+
+class NGramStorage:
+    def add_ngrams(self, list_of_ends):
+        list_of_ngramms = []
+        for i in range(len(list_of_ends[-1])):
+            list_of_ngramms.append(tuple(list_of_ends[j][i] for j in range(len(list_of_ends))))
+        return Counter(list_of_ngramms)
+
+    def __init__(self, sents=[], max_n=0):
+        self.__max_n = max_n
+        self.__ngrams = {i: Counter() for i in range(self.__max_n + 1)}
+
+        # self._ngrams[K] should have the following interface:
+        # self._ngrams[K][(w_1, ..., w_K)] = number of times w_1, ..., w_K occured in words
+        # self._ngrams[0][()] = number of all words
+
+        self.__ngrams[0][()] = np.sum([len(sents[i]) for i in range(len(sents))])
+        tokens = [' '.join(sents[i]) for i in range(len(sents))]
+        tokens = (' '.join(tokens)).split(' ')
+        list_of_ends = []
+        list_of_ends.append(tokens)
+        for n in range(1, self.__max_n + 1):
+            self.__ngrams[n] = add_ngrams(list_of_ends)
+            list_of_ends.append(tokens[n:])
+
+    def ret_ngrams(self):
+        return self.__ngrams
+
+    def add_unk_token(self):
+        """Add UNK token to 1-grams."""
+        # In order to avoid zero probabilites
+        if self.__max_n == 0 or 'UNK' in self.__ngrams[1]:
+            return
+        self.__ngrams[0][()] += 1
+        self.__ngrams[1][('UNK',)] = 1
+
+    @property
+    def max_n(self):
+        """Get max_n"""
+        return self.__max_n
+
+    def __getitem__(self, k):
+        """Get dictionary of k-gram frequencies.
+        Args:
+            k (int): length of returning ngrams' frequencies.
+        Returns:
+            Dictionary (in fact Counter) of k-gram frequencies.
+        """
+        # Cheking the input
+        if not isinstance(k, int):
+            raise TypeError('k (length of ngrams) must be an integer!')
+        if k > self.__max_n:
+            raise ValueError('k (length of ngrams) must be less or equal to the maximal length!')
+        return self.__ngrams[k]
+
+    def __call__(self, ngram):
+        """Return frequency of a given ngram.
+        Args:
+            ngram (tuple): ngram for which frequency should be computed.
+        Returns:
+            Frequency (int) of a given ngram.
+        """
+        # Cheking the input
+        if not isinstance(ngram, tuple):
+            raise TypeError('ngram must be a tuple!')
+        if len(ngram) > self.__max_n:
+            raise ValueError('length of ngram must be less or equal to the maximal length!')
+        if len(ngram) == 1 and ngram not in self.__ngrams[1]:
+            return self.__ngrams[1][('UNK',)]
+        return self.__ngrams[len(ngram)][ngram]
+
+
+final_list_of_ngrams = []
+for ll in link_table.clear_text.values:
+    final_list_of_ngrams.append(nltk.word_tokenize(str(ll)))
+storage = NGramStorage(final_list_of_ngrams, 7)
+ngrams_diseases = storage.ret_ngrams()
+
+final_counter_diseases = Counter()
+for i in range(1, 8):
+    fff = ngrams_diseases[i]
+    for obj_i in fff.most_common(1000):
+        if (',' in obj_i[0]) or ('.' in obj_i[0]) or ('(' in obj_i[0]) or (')' in obj_i[0]) \
+                or ('-' in obj_i[0]) or ('\'' in obj_i[0]) or (':' in obj_i[0]) or (';' in obj_i[0]):
+            pass
+        else:
+            current_obj_i = ' '.join(list(obj_i[0]))
+            final_counter_diseases[' '.join(list(obj_i[0]))] += obj_i[1]
+
+
+def do_morfy(text):
+    new_text = ''
+    for token in nltk.word_tokenize(text):
+        new_text += morfer.parse(token)[0].normal_form + ' '
+    if len(new_text) > 0:
+        new_text = new_text[:-1]
+    return new_text
+
+
+def noraml_elem_of_diseases(x):
+    x = do_morfy(str(x))
+    list_of_symptoms = []
+    tokens = nltk.word_tokenize(x)
+    for i in range(1, 8):
+        for j in range(len(x) - i):
+            if ' '.join(tokens[j: j + i]) in final_counter_diseases:
+                list_of_symptoms.append(
+                    (' '.join(tokens[j: j + i]), final_counter_diseases[' '.join(tokens[j: j + i])]))
+    for_return = sorted(list(np.unique([x[0] for x in list_of_symptoms])), key=lambda x: len(x), reverse=True)
+    return for_return[:int(len(for_return) / 2) + 1]
+
+
+def what_disease(link_table, jaloba_mine):
+    max_score = 0
+    max_disease = ''
+    for i in range(link_table.shape[0]):
+        current_disease = link_table.iloc[i].disease
+        set_symptoms = set(link_table.iloc[i].clear_symptoms)
+        set_jaloba = set(jaloba_mine)
+        inters = set_jaloba.intersection(set_symptoms)
+        score = len(inters) / len(set_jaloba)
+        if (score > max_score) & (len(set_symptoms) > 4):
+            max_score = score
+            max_disease = current_disease
+    if score < 0.1:
+        score *= 5
+    if score > 0.8:
+        score = 0.75
+    return score, max_disease
+###
+print('Ready')
+
+
+HOW_IT_WORKS = emojize(':mag: Как это работает', use_aliases=True)
+ABOUT_US = emojize(':rocket: О нас', use_aliases=True)
+RANDOM_DRUG = emojize(':crying_cat_face: Пример жалобы', use_aliases=True)
+MAIN_MENU_BUTTON = emojize(':seat: Главное меню', use_aliases=True)
+main_menu_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=RANDOM_DRUG)],
+                                                   [KeyboardButton(text=HOW_IT_WORKS), KeyboardButton(text=ABOUT_US)]],
+                                                 resize_keyboard=True)
+back_to_menu_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=MAIN_MENU_BUTTON)]],resize_keyboard=True)
+
+
+def all_work(bot, update, jaloba_mine):
+    bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+
+    try:
+        msg = 'Подождите, пожалуйста, сейчас информация обрабатывается'
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+
+        jaloba_mine = noraml_elem_of_diseases(jaloba_mine)
+        score, result = what_disease(link_table, jaloba_mine)
+        current_tabel = link_table[link_table['disease'] == result]
+        disease = current_tabel.disease.values[0]
+        fealings = current_tabel.fealings.values[0]
+        thinking = current_tabel.thinking.values[0]
+        symptoms = current_tabel.symptoms.values[0]
+
+        if score == 0.0:
+            score = "<0.1"
+        text = "Ваш ближайший диагноз по описанию: {} с вероятностью {}\n".format(disease, score)
+        text += "Наблюдается следущая симптоматика:\n"
+        for i, t in enumerate(symptoms):
+            text += str(i + 1) + ') ' + t + '\n'
+        # text += symptoms
+        text += '\n'
+        try:
+            np.isnan(fealings)
+        except:
+            text += "Сейчас у вас в голове следущие мысли : "
+            text += fealings + '\n'
+            text += "Все будет нормально.\nПо дороге к врачу убеждайте себя и повторяйте, что: "
+            text += thinking
+
+        bot.send_message(chat_id=update.message.chat_id, text=text)
+
+        return MAIN
+
+    except:
+        msg = emojize('Что-то пошло не так :confused:', use_aliases=True)
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+        return MAIN
+
+
+def main_voice(bot, update, user_data):
+    bot.getFile(update.message.voice.file_id).download('voice.ogg')
+
+    audio = AudioSegment.from_file('voice.ogg', format="ogg")
+    audio.export("audio2.raw", format="raw")
+
+    speech_client = speech.Client()
+    with io.open('audio2.raw', 'rb') as audio_file:
+        content = audio_file.read()
+        sample = speech_client.sample(
+            content,
+            encoding='LINEAR16',
+            sample_rate_hertz=48000)
+    try:
+        alternatives = sample.recognize('ru-RU')
+    except Exception as inst:
+        bot.sendMessage(update.message.chat_id, str(inst))
+
+    # for alternative in alternatives:
+    #     bot.sendMessage(update.message.chat_id, alternative.transcript)
+
+    drug = alternatives[0].transcript
+    msg = 'Я распознал вашу речь как ' + drug
+    bot.sendMessage(update.message.chat_id, msg)
+
+    return all_work(bot, update, drug)
+
+
+def main(bot, update, user_data):
+    jaloba_mine = update.message.text
+
+    return all_work(bot, update, jaloba_mine)
+
+
+def start(bot, update, user_data):
+    msg = 'Добрый день, я помощник по определению болезни по сырому тексту жалобы. ' \
+          'Введите жалобу в свободном формате (желательно, без ошибок в орфографии)!'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+
+    return MAIN
+
+
+def random_drug(bot, update):
+    msg = 'Ловите пример жалобы для теста работы'
+    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    msg = 'Жалуюсь на повышение температуры, общую слабость, отёчность и боль при абсцессе'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def print_about_us(bot, update):
+    msg = 'Мы большие молодцы! ФКН ТОП!'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def print_how_it_works(bot, update):
+    msg = 'Из симптомов диагноза извлекается список диагнозов, ' \
+          'из жалобы (сырой текст) вытаскиваются ооооочень умно (Ngramms, нормализация слов, умная токинизация) ' \
+          'тоже список симптомов и вспомогательные переменные, которые помогают найти ближайший диагноз'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def setup(webhook_url=None):
+    global service
+    '''If webhook_url is not passed, run with long-polling.'''
+    updater = Updater(TOKEN)
+    bot = updater.bot
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start, pass_user_data=True)
+    random_drug_handler = RegexHandler(RANDOM_DRUG, random_drug)
+    about_us_handler = RegexHandler(ABOUT_US, print_about_us)
+    how_it_works_handler = RegexHandler(HOW_IT_WORKS, print_how_it_works)
+    main_button_handler = MessageHandler(Filters.text, main, pass_user_data=True)
+    voice_button_handler = MessageHandler(Filters.voice, main_voice, pass_user_data=True)
+    fall_entry = [start_handler, random_drug_handler, about_us_handler, how_it_works_handler,
+                  voice_button_handler, main_button_handler]
+
+    conv_handler = ConversationHandler(
+        entry_points=fall_entry,
+
+        states={
+            MAIN: [RegexHandler(RANDOM_DRUG, random_drug),
+                   RegexHandler(ABOUT_US, print_about_us),
+                   RegexHandler(HOW_IT_WORKS, print_how_it_works),
+                   MessageHandler(Filters.voice, main_voice, pass_user_data=True),
+                   MessageHandler(Filters.text, main, pass_user_data=True)],
+
+            CHOICE: [RegexHandler(MAIN_MENU_BUTTON, main, pass_user_data=True)],
+
+        },
+        fallbacks=fall_entry,
+        allow_reentry=True
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+
+    # dispatcher.add_handler(start_handler)
+    # dispatcher.add_handler(main_button_handler)
+
+    if webhook_url:
+        updater.start_webhook(listen='0.0.0.0',
+                              port=8443,
+                              url_path=TOKEN,
+                              key='private.key',
+                              cert='cert.pem',
+                              webhook_url='https://82.202.246.210:8443/' + TOKEN)
+    else:
+        bot.set_webhook()
+        updater.start_polling()
+        updater.idle()
+
+
+if __name__ == '__main__':
+    setup('')
---
+++ protek_main.py
@@ -0,0 +1,277 @@
+import pandas as pd
+import numpy as np
+from emoji import emojize
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, \
+    ReplyKeyboardMarkup, KeyboardButton, ChatAction, InputMediaPhoto, LabeledPrice
+from telegram.ext import Updater, CommandHandler, ConversationHandler, \
+    MessageHandler, Filters, RegexHandler, CallbackQueryHandler, PreCheckoutQueryHandler
+
+###
+dict_reasons = {
+    'Упаковка': 1,
+    'инструкция турецком языке': 1,
+
+    'Маркировка инструкция': 2,
+    'Маркировка ампулах стерта': 2,
+    'Маркировка': 2,
+
+    'Отзыв декларации соответствии': 5,
+    'Отзыв производителем': 5,
+    'Отмена государственной регистрации': 5,
+    'Прекращение действия декларации соответствии': 5,
+    'Отзыв производителем декларации соответствии': 5,
+    'связи преостановлением реализации фармацевтической субстанции': 5,
+    'Необходимость гармонизации регистрационного досье нормативной документации': 5,
+    'Необходимость гармонизации регистрационных документов': 5,
+    'Отмена действия сертификата соответствия': 5,
+
+    'Описание (': 6,
+    'Описание,': 6,
+    'Цветность': 6,
+    'запись отсутствует Государственном реестре': 6,
+    'запись о которой отсутствует в Государственном реестре лекарственных средств': 6,
+
+    'Количественное определение': 8,
+    'Герметичность': 8,
+    'Растворение': 8,
+
+    'Микробиологическая чистота': 10,
+
+    'Фальсификация': 10,
+    'Подлинность': 10,
+    'Признаки фальсификации': 10,
+    'Отмена действия сертификата соответствия': 10,
+    'этикетках флаконов отсутствуют серия срок годности': 10,
+    'Указание первичной вторичной упаковках': 10,
+    'Стабильность препарата': 10,
+    'Развитие нежелательных реакций': 10,
+    'Растворение': 10,
+    'Несоответствие процессе производства': 10,
+    'Выявление разночтений документах': 10,
+    'Выявлены несоответствия утвержденным нормам спецификации изучения стабильности': 10,
+    'Механические включения': 10,
+    'Обнаружение частиц полиуретана упаковочном оборудовании': 10,
+    'Остаточные органические растворители': 10,
+
+    'Показатель письме указан': 1,
+    'Посторонние примеси': 10,
+    'Развитие нежелательных реакций': 10
+}
+
+med_final = pd.read_csv('med_final.csv', index_col=0)
+med_names = pd.read_csv('med_names.csv', index_col=0)
+med_prep = pd.read_csv('med_prep.csv', index_col=0)
+
+dict_med_names = {}
+for i in range(len(med_names)):
+    if med_names.loc[i, 'ТН'] not in dict_med_names:
+        dict_med_names[med_names.loc[i, 'ТН']] = med_names.loc[i, 'МНН']
+
+dict_tn_prod = {}
+for i in range(len(med_final)):
+    if med_final.loc[i, 'ТН'] not in dict_tn_prod:
+        dict_tn_prod[med_final.loc[i, 'ТН']] = med_final.loc[i, 'Производитель']
+
+dict_tn_prod_all = {}
+for i in range(len(med_prep)):
+    if med_prep.loc[i, 'Торговое наименование\nлекарственного препарата'] not in dict_tn_prod_all:
+        dict_tn_prod_all[med_prep.loc[i, 'Торговое наименование\nлекарственного препарата']] = med_prep.loc[
+            i, 'Юридическое лицо, на имя которого выдано регистрационное удостоверение']
+
+dict_producers = {}
+for i in range(len(med_final)):
+    if med_final.loc[i, 'Производитель'] not in dict_producers:
+        #         print(dict_reasons[med_final.loc[i, 'Причина']])
+        dict_producers[med_final.loc[i, 'Производитель']] = dict_reasons[med_final.loc[i, 'Причина']]
+    else:
+        dict_producers[med_final.loc[i, 'Производитель']] += dict_reasons[med_final.loc[i, 'Причина']]
+
+dict_producers_cnt = {}
+for i in range(len(med_final)):
+    if med_final.loc[i, 'Производитель'] not in dict_producers_cnt:
+        #         print(dict_reasons[med_final.loc[i, 'Причина']])
+        dict_producers_cnt[med_final.loc[i, 'Производитель']] = 1
+    else:
+        dict_producers_cnt[med_final.loc[i, 'Производитель']] += 1
+
+
+def sorted_producers(medicine_name):
+    medicine_names = list(med_names[med_names['МНН'] == medicine_name]['ТН'].unique())
+    medicine_names2 = list(med_final[med_final['МНН'] == medicine_name]['ТН'].unique())
+    medicine_names += medicine_names2
+    rating = []
+    for name in medicine_names:
+        if name in dict_tn_prod:
+            prod = dict_tn_prod[name]
+            if prod in dict_producers:
+                rating.append((name, prod, np.round(dict_producers[prod] / dict_producers_cnt[prod], 2)))
+        elif name in dict_tn_prod_all:
+            prod = dict_tn_prod_all[name]
+            rating.append((name, prod, 0))
+        else:
+            rating.append((name, '-', 0))
+    rating = sorted(rating, key=lambda x: x[2])
+    return rating
+
+
+def all_rating():
+    rating = []
+    for name in dict_producers.keys():
+        if name in dict_producers_cnt:
+            rating.append((name, np.round(dict_producers[name]/dict_producers_cnt[name], 2)))
+    rating = sorted(rating, key=lambda x: x[1])
+    return rating
+
+
+top_producers = all_rating()
+print('Ready')
+###
+
+TOKEN = '570995498:AAGcQZNuBJv8WrEr7Zy0Q3LDDCpoLU3wH9U'
+
+MAIN, CHOICE = range(2)
+
+# data = pd.read_csv('med_df_text.csv', usecols=['ТН', 'Производитель'])
+# data.dropna(subset=['ТН', 'Производитель'], inplace=True)
+# data.drop_duplicates(inplace=True)
+# data.reset_index(inplace=True, drop=True)
+# data = data[['ТН', 'Производитель']]
+
+HOW_IT_WORKS = emojize(':mag: Как это работает', use_aliases=True)
+ABOUT_US = emojize(':rocket: О нас', use_aliases=True)
+RANDOM_DRUG = emojize(':pill: Тестовое лекарство', use_aliases=True)
+TOP_PRODUCERS = emojize(':star: Лучшие производители', use_aliases=True)
+MAIN_MENU_BUTTON = emojize(':seat: Главное меню', use_aliases=True)
+main_menu_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=TOP_PRODUCERS)], [KeyboardButton(text=RANDOM_DRUG)],
+                                                   [KeyboardButton(text=HOW_IT_WORKS), KeyboardButton(text=ABOUT_US)]],
+                                                 resize_keyboard=True)
+back_to_menu_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=MAIN_MENU_BUTTON)]],resize_keyboard=True)
+
+
+def all_work(bot, update, drug):
+    bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+
+    try:
+        msg = 'Подождите, пожалуйста, сейчас информация обрабатывается'
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+
+        current_drug = sorted_producers(drug)
+
+        msg = ''
+        if len(current_drug) == 1:
+            msg = 'Only one producer: ' + current_drug[0][0]
+        else:
+            msg = 'There are several producers:'
+            for k in range(len(current_drug)):
+                msg += '\n' + str(k + 1) + '. ' + current_drug[k][0]
+
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+
+        return MAIN
+
+    except:
+        msg = emojize('Что-то пошло не так :confused:', use_aliases=True)
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+        return MAIN
+
+
+def main(bot, update, user_data):
+    drug = update.message.text
+
+    return all_work(bot, update, drug)
+
+
+def start(bot, update, user_data):
+    msg = 'Вы можете отправить мне название лекарства, а я выдам самых качественных производителей. ' \
+          'Также, я могу показать список самых надежных производителей! \n\nПример лекарства:'
+    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    msg = 'Валерианы настойка'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+
+    return MAIN
+
+
+def random_drug(bot, update):
+    msg = 'Ловите лекарство из нашей базы для теста работы'
+    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    # n = np.random.random_integers(1, data.shape[0]) - 1
+    # msg = data['ТН'][n]
+    msg = 'Дротаверин'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def print_about_us(bot, update):
+    msg = 'Мы большие молодцы! ФКН ТОП!'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def print_how_it_works(bot, update):
+    msg = 'Вот как это работает!'
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def print_top_producers(bot, update):
+    msg = 'ТОП производителей таков:'
+    temp = top_producers
+    for k in range(10):
+        msg += '\n' + str(k+1) + '. ' + temp[k][0]
+    bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=main_menu_keyboard)
+    return MAIN
+
+
+def setup(webhook_url=None):
+    global service
+    '''If webhook_url is not passed, run with long-polling.'''
+    updater = Updater(TOKEN)
+    bot = updater.bot
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start, pass_user_data=True)
+    random_drug_handler = RegexHandler(RANDOM_DRUG, random_drug)
+    top_producers_handler = RegexHandler(TOP_PRODUCERS, print_top_producers)
+    about_us_handler = RegexHandler(ABOUT_US, print_about_us)
+    how_it_works_handler = RegexHandler(HOW_IT_WORKS, print_how_it_works)
+    main_button_handler = MessageHandler(Filters.text, main, pass_user_data=True)
+    fall_entry = [start_handler, top_producers_handler, random_drug_handler, about_us_handler, how_it_works_handler,
+                  main_button_handler]
+
+    conv_handler = ConversationHandler(
+        entry_points=fall_entry,
+
+        states={
+            MAIN: [RegexHandler(RANDOM_DRUG, random_drug),
+                   RegexHandler(ABOUT_US, print_about_us),
+                   RegexHandler(HOW_IT_WORKS, print_how_it_works),
+                   MessageHandler(Filters.text, main, pass_user_data=True)],
+
+            CHOICE: [RegexHandler(MAIN_MENU_BUTTON, main, pass_user_data=True)],
+
+        },
+        fallbacks=fall_entry,
+        allow_reentry=True
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+
+    # dispatcher.add_handler(start_handler)
+    # dispatcher.add_handler(main_button_handler)
+
+    if webhook_url:
+        updater.start_webhook(listen='0.0.0.0',
+                              port=8443,
+                              url_path=TOKEN,
+                              key='private.key',
+                              cert='cert.pem',
+                              webhook_url='https://82.202.246.210:8443/' + TOKEN)
+    else:
+        bot.set_webhook()
+        updater.start_polling()
+        updater.idle()
+
+
+if __name__ == '__main__':
+    setup('')
